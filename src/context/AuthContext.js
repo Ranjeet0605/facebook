42649a256544454c367900777897a96cc4465d83
Ranjeet0605 <111526289+Ranjeet0605@users.noreply.github.com>
2023-07-15 19:11:39 +0530

    Update AuthContext.js
     authcontext change

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,30 +1,7 @@
 import { createContext,useReducer } from "react";
 import AuthRuducer from "./AuthReduce";
 const INITIAL_STATE = { 
-    user:{
-      "_id": 
-         "64119227aaab2a4bfdc724da",
-      "username": "Samantha",
-      "email": "samantha123@gmial.com",
-      "password": "$2b$10$HGHy4Rhqroi3TZ0lNKj5DOe8frQ0e4ZUJ7mnbpGfaPYCpOpwMBD9K",
-      "profilePicture": "person/gril.jpg",
-      "coverPicture": "post/3.jpeg",
-      "followers": [
-        "64a29c46ee3081031e738562",
-      ],
-      "isAdmin": false,
-      "createdAt": {
-        "$date": "2023-03-15T09:38:47.040Z"
-      },
-      "updatedAt": {
-        "$date": "2023-03-16T16:47:26.210Z"
-      },
-      "__v": 0,
-      "followings": [
-    
-      ]
-    },
-           
+    user: null,    
     isFetching: false,
     error:false,
 };
